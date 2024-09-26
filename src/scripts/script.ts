c006c3504ec2f6b1217fc7d89aab4436169a4449
Rohit Sharma <rohitsharma@gkmit.co>
2024-09-26 15:29:09 +0530

    feat: addDeveloper

---
+++ src/scripts/script.ts
@@ -0,0 +1,13 @@
+interface project{
+    projectName: string;
+    isCompleted: boolean;
+    techStack: string[];
+}
+interface developer{
+    name: string;
+    age: number;
+    isEmployed: boolean;
+    skills: string[];
+    projects: project[];
+    experience: number;
+}
